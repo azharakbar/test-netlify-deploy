e7af48b83db14be835eaeeaf6f7c7597e9943674
Mohammed Azhar <azharakbar@gmail.com>
2021-11-08 12:30:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.MY_SECRET} Yay!! Edit <code>src/App.js</code> and save to reload.
+          {process.env.MY_SECRET} Yay!! From GA Axns!! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
