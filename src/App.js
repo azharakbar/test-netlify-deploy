457e005f1d1fd424988fc0509faa32119b1aecd8
Mohammed Azhar <azharakbar@gmail.com>
2021-11-14 17:56:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.MY_SECRET} Yay!! From GA Axns!! Edit <code>src/App.js</code> and save to reload.
-          <br />
-            Cool Deployments
-            <br />
-              Stopped builds in netlify
-<br /> Changing again to create new commit <br />
-Super Cool Deployments
-            <br />
-  All done ??? 
-            <br />
+          Cleanup !!!
         </p>
         <a
           className="App-link"
